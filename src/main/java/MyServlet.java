79aa3a00bedb53163b2efa0d56dbf72db18b0482
oleg.maliy14@gmail.com <q2301299266>
2019-09-03 13:07:53 +0300

    -0- 03.09.2019

---
+++ src/main/java/MyServlet.java
@@ -0,0 +1,29 @@
+package main.java;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+
+        pw.println("<html>");
+        pw.println("<h1> Test! </h1>");
+        pw.println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
